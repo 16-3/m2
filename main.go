60acb822203f64e298a603ad9d83b60d4c3a972e
16-3 <118816150+16-3@users.noreply.github.com>
2023-05-10 17:37:09 +0900

    Update main.go

--- main.go
+++ main.go
@@ -2,17 +2,41 @@ package main
 
 import (
 	"context"
+	"encoding/json"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"strings"
 
+	"github.com/chromedp/cdproto/page"
 	"github.com/chromedp/chromedp"
 )
 
+type PostBody struct {
+	URL string `json:"url"`
+}
+
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		url := r.URL.Query().Get("url")
-		if url == "" {
+		if r.Method != http.MethodPost {
+			http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+			return
+		}
+
+		body, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			http.Error(w, "Error reading request body", http.StatusInternalServerError)
+			return
+		}
+
+		var postData PostBody
+		err = json.Unmarshal(body, &postData)
+		if err != nil {
+			http.Error(w, "Error parsing JSON", http.StatusBadRequest)
+			return
+		}
+
+		if postData.URL == "" {
 			http.Error(w, "Missing URL", http.StatusBadRequest)
 			return
 		}
@@ -21,8 +45,8 @@ func main() {
 		defer cancel()
 
 		var html string
-		err := chromedp.Run(ctx,
-			chromedp.Navigate(url),
+		err = chromedp.Run(ctx,
+			chromedp.Navigate(postData.URL),
 			chromedp.ScrollIntoView(`document.body`, chromedp.ByJSPath),
 			chromedp.OuterHTML("html", &html),
 		)
@@ -33,6 +57,48 @@ func main() {
 
 		w.Header().Set("Content-Type", "text/html; charset=utf-8")
 		strings.NewReader(html).WriteTo(w)
+	})
+
+	http.HandleFunc("/img", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPost {
+			http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+			return
+		}
+
+		body, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			http.Error(w, "Error reading request body", http.StatusInternalServerError)
+			return
+		}
+
+		var postData PostBody
+		err = json.Unmarshal(body, &postData)
+		if err != nil {
+			http.Error(w, "Error parsing JSON", http.StatusBadRequest)
+			return
+		}
+
+		if postData.URL == "" {
+			http.Error(w, "Missing URL", http.StatusBadRequest)
+			return
+		}
+
+		ctx, cancel := chromedp.NewContext(context.Background())
+		defer cancel()
+
+		var screenshot []byte
+		err = chromedp.Run(ctx,
+			chromedp.Navigate(postData.URL),
+			chromedp.ScrollIntoView(`document.body`, chromedp.ByJSPath),
+			chromedp.CaptureScreenshot(&screenshot),
+		)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set("Content-Type", "image/png")
+		w.Write(screenshot)
 	})
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
